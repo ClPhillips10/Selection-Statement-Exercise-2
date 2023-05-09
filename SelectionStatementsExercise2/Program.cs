0c178ce6a10ebe297c2a033f66044a40f6b7f5ec
ClPhillips10 <Lakephillips3232@gmail.com>
2023-05-09 10:21:27 -0500

    Favorite class switch cases

--- SelectionStatementsExercise2/Program.cs
+++ SelectionStatementsExercise2/Program.cs
@@ -4,7 +4,38 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
-        }
+            Console.Write("What is your favorite school subject? ");
+            var FavSubj = Console.ReadLine();
+            
+
+            switch (FavSubj.ToLower())
+            {
+                case "math":
+                    Console.WriteLine("That is the only right answer");
+                    break;
+                case "science":
+                    Console.WriteLine($"{FavSubj.ToUpper()} Is okay but not my favorite");
+                    break;
+                case "history":
+                    Console.WriteLine($"I dont like {FavSubj} very much.");
+                    break;
+                case "english":
+                    Console.WriteLine("Ingles es muy difícil");
+                    break;
+                case "lunch":
+                    Console.WriteLine("This is everyones favorite");
+                    break;
+                default:
+                    Console.WriteLine($"I've never heard of {FavSubj} before");
+                    break;
+
+
+
+
+            }
+
+            }
+
+
     }
 }
